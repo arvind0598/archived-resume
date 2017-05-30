d08ff52270db30f0d35d25f354c0a6bc41aade6a
Arvind S <arvind0598@gmail.com>
2017-05-31 02:05:54 +0530

    Add files via upload
    Huge redesign. The welcome screen has been added. Everything recolored. Social bar moved upward. So much more.

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function()
+{
+
+  $("#button_intro").click(function()
+  {
+    $("#intro").css("background-color", "#70a8a6"); //dark grey
+    $("body").css("background-color", "#74AFAD"); //light grey
+    $("#intro").slideUp(1000);
+    $("section").toggleClass("not_intro");
+    $("#contact").fadeOut(1000);
+  });
+
+  $("#button_up").click(function()
+  {
+    $("body").css("background-color", "#303F9F"); //dark indigo
+    $("#intro").css("background-color", "#3F51B5"); //light indigo
+    $(".not_intro").slideDown(1000);
+    $("section").toggleClass("not_intro");
+    $("#contact").fadeIn(1000);
+  });
+
+});
