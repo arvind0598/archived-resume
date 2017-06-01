047f2101bea58ee1993c4485b2a4d3f3e86e88f0
Arvind S <arvind0598@gmail.com>
2017-06-02 02:09:12 +0530

    Add files via upload
    Removed buttons. Implemented smooth scroll and anchor links. Fancy learn more button. Rearranged boxes. Split CSS files as main and mobile. Added comments and more vendor prefixes in main.css

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 $(document).ready(function()
 {
-
-  $("#button_intro").click(function()
+  $("a").on('click', function(event)
   {
-    $("#intro").css("background-color", "#70a8a6"); //dark body
-    $("body").css("background-color", "#74AFAD"); //light body
-    $("#intro").slideUp(1000);
-    $("section").toggleClass("not_intro");
-    $("#contact").fadeOut(1000);
+    if (this.hash !== "")
+    {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function()
+      {
+        window.location.hash = hash;
+      });
+    }
   });
-
-  $("#button_up").click(function()
-  {
-    $("body").css("background-color", "#303F9F"); //dark indigo
-    $("#intro").css("background-color", "#3F51B5"); //light indigo
-    $(".not_intro").slideDown(1000);
-    $("section").toggleClass("not_intro");
-    $("#contact").fadeIn(1000);
-  });
-
 });
