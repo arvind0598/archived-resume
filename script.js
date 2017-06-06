727cc90eab03dff969c2d97efeee51119256c814
Arvind S <arvind0598@gmail.com>
2017-06-07 03:45:00 +0530

    Huge Changes.
    Multiple pages. 
    Navigation menu. 
    No Social icons. Should load faster. 
    Everything looks better. 
    Redesigned mobile.css from ground up.

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 $(document).ready(function()
 {
+
   $("a").on('click', function(event)
   {
     if (this.hash !== "")
     {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function()
+      event.preventDefault(); //prevents actual behaviour
+      var hash = this.hash; //saves link
+      $('html, body').animate({scrollTop: $(hash).offset().top}, 1000, function() //actual animation
       {
-        window.location.hash = hash;
+        window.location.hash = hash; //equates link
       });
     }
   });
+
+  if($(window).width() > 700)
+  {
+    $(".hover").hover(function()
+    {
+      $("#anchors").toggleClass("close");
+      $("#site_wrap").toggleClass("close");
+    });
+  }
+  else
+  {
+    $(".arrow").hover(function()
+    {
+      $("#anchors").toggleClass("close");
+      $("#site_wrap").toggleClass("close");
+    });
+  }
+
 });
